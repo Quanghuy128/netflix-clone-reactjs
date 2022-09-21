ac061992e244aecb38ffe7b3bc5a0f0af52e509a
unknown <quanghuyhihi128@gmail.com>
2022-09-21 13:31:02 +0700

    [Update-03-04] - Row.jsx - Handle slide button

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -5,9 +5,8 @@ import Movie from '../components/Movie'
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowID }) => {
     const [movies, setMovies] = useState([]);
-
 
     useEffect(() => {
         axios.get(fetchURL).then((res) => {
@@ -15,22 +14,31 @@ const Row = ({ title, fetchURL }) => {
         })
 
     }, [fetchURL]);
-    console.log(movies)
+
+    const sideLeft = () => {
+        var slider = document.getElementById('slider' + rowID);
+        slider.scrollLeft = slider.scrollLeft - 500;
+    }
+    const sideRight = () => {
+        var slider = document.getElementById('slider' + rowID);
+        slider.scrollLeft = slider.scrollLeft + 500;
+    }
+
     return (
         <div>
             <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
 
             <div className='relative flex items-center group'>
-                <MdChevronLeft
+                <MdChevronLeft onClick={sideLeft}
                     className='bg-white left-0 h-[70%] absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
                     size={40}
                 />
-                <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
+                <div id={'slider' + rowID} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
                     {movies.map((item, id) => (
                         <Movie key={id} item={item} />
                     ))}
                 </div>
-                <MdChevronRight
+                <MdChevronRight onClick={sideRight}
                     className='bg-white right-0 h-[70%] absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
                     size={40} />
             </div>
